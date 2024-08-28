6981a4b57c9c95c72ebf3022a1a46c9db7d52a9e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 06:39:44 +0330

    - Create Propertis In Transaction Class

--- ATMConsole/Transaction.vb
+++ ATMConsole/Transaction.vb
@@ -7,5 +7,16 @@ End Enum
 
 
 Public Class Transaction
+    Public Property TransactionId As Integer
 
+    Public Property BankAccountNoFrom As Long
+
+
+    Public Property BankAccountNoTo As Long
+
+    Public Property TransactionType As TransactionType
+    Public Property TransactionAmount As Decimal
+
+
+    Public Property TransactionDate As Date
 End Class
