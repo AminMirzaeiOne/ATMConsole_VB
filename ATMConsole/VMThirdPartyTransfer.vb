ef903fe88c0c70ba64d37d9580c4e20b71a12638
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 06:49:30 +0330

    - Create and Add Propertis In VMThirdPartyTransfer Class

--- ATMConsole/VMThirdPartyTransfer.vb
+++ ATMConsole/VMThirdPartyTransfer.vb
@@ -1,3 +1,6 @@
 ﻿Public Class VMThirdPartyTransfer
+    Public Property TransferAmount As Decimal
+    Public Property RecipientBankAccountNumber As Long
 
+    Public Property RecipientBankAccountName As String
 End Class
