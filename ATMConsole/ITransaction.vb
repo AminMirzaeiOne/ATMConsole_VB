8d1e26a163e3628ab7bd86575e6fdc031e9a31b7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 05:53:55 +0330

    - Update ITransaction Interface Add Functions

--- ATMConsole/ITransaction.vb
+++ ATMConsole/ITransaction.vb
@@ -1,3 +1,5 @@
 ﻿Public Interface ITransaction
+    Sub InsertTransaction(bankAccount As BankAccount, transaction As Transaction)
 
+    Sub ViewTransaction(bankAccount As BankAccount)
 End Interface
