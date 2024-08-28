07ad916c06c96d2815dd12ccc3166f49058eecde
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 06:41:59 +0330

    - Create GetValidDecimalInputAmt And GetValidIntInputAmt Function

--- ATMConsole/Utility.vb
+++ ATMConsole/Utility.vb
@@ -1,3 +1,38 @@
-﻿Public Class Utility
+﻿Imports System.Globalization
+
+Public Class Utility
+    Private Shared culture As CultureInfo = New CultureInfo("ms-MY")
+
+    Public Shared Function GetValidDecimalInputAmt(input As String) As Decimal
+        Dim valid = False
+        Dim rawInput As String
+        Dim amount As Decimal = 0
+
+        ' Get user's input input type is valid
+        While Not valid
+            rawInput = GetRawInput(input)
+            valid = Decimal.TryParse(rawInput, amount)
+            If Not valid Then PrintMessage("Invalid input. Try again.", False)
+        End While
+
+        Return amount
+    End Function
+
+    Public Shared Function GetValidIntInputAmt(input As String) As Long
+        Dim valid = False
+        Dim rawInput As String
+        Dim amount As Long = 0
+
+        ' Get user's input input type is valid
+        While Not valid
+            rawInput = GetRawInput(input)
+            valid = Long.TryParse(rawInput, amount)
+            If Not valid Then PrintMessage("Invalid input. Try again.", False)
+        End While
+
+        Return amount
+    End Function
+
+
 
 End Class
