bdc7bcefad2fca1a8cb5622bb0484babfb834247
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 05:44:58 +0330

    - Create and Add Propertis

--- ATMConsole/BankAccount.vb
+++ ATMConsole/BankAccount.vb
@@ -1,3 +1,9 @@
 ﻿Public Class BankAccount
+    Public Property FullName As String
+    Public Property AccountNumber As Long
+    Public Property CardNumber As Long
+    Public Property PinCode As Long
+    Public Property Balance As Decimal
 
+    Public Property isLocked As Boolean
 End Class
