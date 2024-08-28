90b23f5fa39c0261b25ceaa939619e3839d8f2a8
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-28 05:58:11 +0330

    - Implementation of inheritance of interfaces in MeybankATM class

--- ATMConsole/MeybankATM.vb
+++ ATMConsole/MeybankATM.vb
@@ -1,3 +1,31 @@
 ﻿Public Class MeybankATM
+    Implements ILogin, IBalance, IDeposit, IWithdrawal, IThirdPartyTransfer, ITransaction
 
+    Public Sub InsertTransaction(bankAccount As BankAccount, transaction As Transaction) Implements ITransaction.InsertTransaction
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub ViewTransaction(bankAccount As BankAccount) Implements ITransaction.ViewTransaction
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub PerformThirdPartyTransfer(bankAccount As BankAccount, vmThirdPartyTransfer As VMThirdPartyTransfer) Implements IThirdPartyTransfer.PerformThirdPartyTransfer
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub MakeWithdrawal(bankAccount As BankAccount) Implements IWithdrawal.MakeWithdrawal
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub CheckCardNoPassword() Implements ILogin.CheckCardNoPassword
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub CheckBalance(bankAccount As BankAccount) Implements IBalance.CheckBalance
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub PlaceDeposit(bankAccount As BankAccount) Implements IDeposit.PlaceDeposit
+        Throw New NotImplementedException()
+    End Sub
 End Class
