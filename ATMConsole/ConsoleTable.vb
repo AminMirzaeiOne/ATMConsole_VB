c5ed028f7a0b3ba43c62ab8db3f95a6cec6ded5a
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-30 07:23:31 +0330

    - Create NumericTypes And Constractor Class

--- ATMConsole/ConsoleTable.vb
+++ ATMConsole/ConsoleTable.vb
@@ -2,5 +2,37 @@
     Public ReadOnly Property Columns As IList(Of Object)
     Public ReadOnly Property Rows As IList(Of Object())
 
+    Private _ColumnTypes As System.Type(), _Formats As System.Collections.Generic.IList(Of String)
+    Public ReadOnly Property Options As ConsoleTableOptions
+
+    Public Property ColumnTypes As Type()
+        Get
+            Return _ColumnTypes
+        End Get
+        Private Set(value As Type())
+            _ColumnTypes = value
+        End Set
+
+    Public Shared ReadOnly NumericTypes As HashSet(Of Type) = New HashSet(Of Type) From {
+    GetType(Integer),
+        GetType(Double),
+        GetType(Decimal),
+    GetType(Long),
+        GetType(Short),
+        GetType(SByte),
+    GetType(Byte),
+        GetType(ULong),
+        GetType(UShort),
+    GetType(UInteger),
+        GetType(Single)
+    }
+
+    Public Sub New(ParamArray columns As String())
+        Me.New(New ConsoleTableOptions With {
+                .columns = New List(Of String)(columns)
+            })
+    End Sub
+
+
 
 End Class
